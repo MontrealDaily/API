8e38be8175be6d64e024f301c9c33ec53a8c23d7
Alexandre <alexandre@brde.fr>
2023-08-14 10:57:26 +0200

    Init API project

---
+++ src/test/java/fr/brde/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.brde.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
